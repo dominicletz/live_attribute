5bae434e7367c19cb3c9e7ab25eb5376d984ae6f
Dominic Letz <dominic@diode.io>
2022-02-04 13:59:15 +0100

    Fixed extra_applications

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule LiveAttribute.MixProject do
   use Mix.Project
 
-  @version "1.0.1"
+  @version "1.0.2"
   @name "LiveAttribute"
   @url "https://github.com/dominicletz/live_attribute"
   @maintainers ["Dominic Letz"]
@@ -54,7 +54,7 @@ defmodule LiveAttribute.MixProject do
   end
 
   def application do
-    [extra_applications: [:global]]
+    [extra_applications: []]
   end
 
   defp deps do
