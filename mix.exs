d8335bb4e21603c7c76e4612d7d36ef335b309b4
Dominic Letz <dominic.letz@berlin.de>
2025-02-18 10:11:55 +0100

    v1.1.5

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule LiveAttribute.MixProject do
   use Mix.Project
 
-  @version "1.1.4"
+  @version "1.1.5"
   @name "LiveAttribute"
   @url "https://github.com/dominicletz/live_attribute"
   @maintainers ["Dominic Letz"]
